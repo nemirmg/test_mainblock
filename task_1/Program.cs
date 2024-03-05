7fbfac352ca4c8bada3b8452beaa48b9d5f972ea
Mihail Nemirovchenko <nemirovchenko@gmail.com>
2024-03-05 20:50:25 +0300

    Добавил решение задачи (Program.cs)

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayStr = new string[] {"Hello", "2", "world", ":-)"};
+
+int count = 0;
+foreach (string element in arrayStr)
+{
+    if (element.Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] newArrStr = new string[count];
+
+if (count > 0)
+{
+    int j = 0;
+    foreach (string element in arrayStr)
+    {
+        if (element.Length <= 3)
+        {
+            newArrStr[j] = element;
+            j++;
+        }
+    }
+}
+
+Console.WriteLine($"[{string.Join(", ", arrayStr)}] -> [{string.Join(", ", newArrStr)}]");
